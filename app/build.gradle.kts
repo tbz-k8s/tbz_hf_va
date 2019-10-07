9390d30f28b73d4dd98fc24bc3873b33a8049913
Niklas Liechti <niklas.liechti@ergon.ch>
2019-10-07 16:21:19 +0200

    Check in ide config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,11 @@ dependencies {
     implementation("io.javalin:javalin:3.5.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.10.0")
     implementation("org.slf4j:slf4j-simple:1.7.28")
+
+    // Web stuff
     implementation("org.webjars.npm:vue:2.6.10")
-    
+    compile("org.webjars.npm:vuetify:2.1.1")
+
     implementation("io.fabric8:kubernetes-client:4.6.0")
     implementation("io.fabric8:kubernetes-model:4.6.0")
 }
