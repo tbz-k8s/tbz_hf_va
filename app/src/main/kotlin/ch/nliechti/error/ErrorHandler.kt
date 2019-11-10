2c63841f6d2da38531deccfe625fba0dda015bd2
Nikals Liechti <nliechti@nliechti.ch>
2019-11-10 16:30:01 +0100

    Show kubernetes client message if something went wrong

--- app/src/main/kotlin/ch/nliechti/error/ErrorHandler.kt
+++ app/src/main/kotlin/ch/nliechti/error/ErrorHandler.kt
@@ -1,5 +1,6 @@
 package ch.nliechti.error
 
+import io.fabric8.kubernetes.client.KubernetesClientException
 import io.javalin.Javalin
 import io.javalin.plugin.rendering.vue.VueComponent
 
@@ -7,4 +8,8 @@ import io.javalin.plugin.rendering.vue.VueComponent
 fun addErrorHandler(app: Javalin) {
     app.error(404, "html", VueComponent("<not-found></not-found>"))
     app.error(500, "html", VueComponent("<internal-server-error></internal-server-error>"))
+
+    app.exception(KubernetesClientException::class.java) { e, ctx ->
+        ctx.res.sendError(500, e.status.message)
+    }
 }
